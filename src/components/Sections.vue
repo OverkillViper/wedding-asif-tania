607f8d2dc5dd3cb6ddbf52af65cbcc4bb8774ad3
OverkillViper <117332017+OverkillViper@users.noreply.github.com>
2025-01-16 15:46:57 +0600

    Update Sections.vue

--- src/components/Sections.vue
+++ src/components/Sections.vue
@@ -13,10 +13,6 @@
                 <div class="absolute font-quicksand font-semibold m-2 md:m-4 text bg-[#6f473d] text-sm md:text-xl uppercase text-white px-3 md:py-2 py-2 md:px-4 rounded-md">Bride & Groom</div>
                 <img src="../assets/images/bride_groom.png" alt="invitation_image"   class="w-full h-full object-cover">
             </router-link>
-            <router-link :to="{ name: 'Wedding' }"          class="item max-h-36 md:max-h-96 w-38 md:w-40 relative">
-                <div class="absolute font-quicksand font-semibold m-2 md:m-4 text bg-[#6f473d] text-sm md:text-xl uppercase text-white px-3 md:py-2 py-2 md:px-4 rounded-md">Wedding</div>
-                <img src="../assets/images/wedding.jpg" alt="invitation_image"       class="w-full h-full object-cover">
-            </router-link>
             <router-link :to="{ name: 'Reception' }"        class="item max-h-36 md:max-h-96 w-38 md:w-40 relative">
                 <div class="absolute font-quicksand font-semibold m-2 md:m-4 text bg-[#6f473d] text-sm md:text-xl uppercase text-white px-3 md:py-2 py-2 md:px-4 rounded-md">Reception</div>
                 <img src="../assets/images/reception.jpg" alt="invitation_image"     class="w-full h-full object-cover">
